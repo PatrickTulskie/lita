b5cfe0a5095fc2d9d55ccbab4dc3510357c0a785
Patrick Tulskie <patricktulskie@gmail.com>
2013-07-23 08:57:00 -0400

    Modifying some of the log handling when daemonized.

--- lib/lita/cli.rb
+++ lib/lita/cli.rb
@@ -26,43 +26,47 @@ module Lita
     class_option :pid_file,
       aliases: "-p",
       banner: "PID_FILE",
-      default: "/tmp/lita.pid",
+      default: "/var/run/lita.pid",
       desc: "Location for pid files when daemonized"
     
     class_option :stdout,
       aliases: "-o",
       banner: "STDOUT",
-      default: "/tmp/lita.stdout.log",
+      default: "/var/log/lita.stdout.log",
       desc: "Where to direct stdout"
       
     class_option :stderr,
       aliases: "-e",
       banner: "STDERR",
-      default: "/tmp/lita.stderr.log",
+      default: "/var/log/lita.stderr.log",
       desc: "Where to direct stderr"
       
     desc "start", "Starts Lita"
     def start
       Bundler.require
-      
-      if options[:daemon]
-        $0 = "lita_bot"
-
-        # Spawn a deamon for our bot
-        Lita::Daemon.start(fork, :pid_file => options[:pid_file], :stdout_file => options[:stdout], :stderr_file => options[:stderr])
-
-        # Set up signals for our daemon so it knows how to exit
-        Signal.trap("HUP", "IGNORE")
-        Signal.trap("INT", "IGNORE")
-        Signal.trap("QUIT") { $stdout.puts "SIGQUIT and exit"; exit }
-      end
-      
+      setup_daemon if options[:daemon]
       Lita.run(options[:config])
     end
 
     desc "new NAME", "Generates a new Lita project (default name: lita)"
     def new(name = "lita")
       directory "skeleton", name
+    end
+    
+    private
+    
+    def setup_daemon
+      # Rename the process so it's obvious in process lists
+      $0 = "lita_bot"
+
+      # Spawn a deamon for our bot
+      Lita::Daemon.start(fork, :pid_file => options[:pid_file], :stdout_file => options[:stdout], :stderr_file => options[:stderr])
+
+      # Set up signals for our daemon so it knows how to exit
+      Signal.trap("QUIT") { exit }
+      %w(HUP INT).each do |signal|
+        Signal.trap(signal, "IGNORE")
+      end
     end
     
   end
--- lib/lita/daemon.rb
+++ lib/lita/daemon.rb
@@ -6,20 +6,27 @@ module Lita
       stdout_file = options[:stdout_file] || "/tmp/lita.stdout.log"
       stderr_file = options[:stderr_file] || "/tmp/lita.stderr.log"
       
-      unless pid.nil?
-        raise "Fork failed" if pid == -1
-        # Make sure that there are no existing processes using an
-        # existing pid file
+      case pid
+      when nil # Nil for the fork value means we're in the child process
+        redirect_streams(stdout_file, stderr_file)
+      when -1 # Couldn't fork for some reason - usually OS related
+        raise "Forking failed for some reason.  Does this OS support forking?"
+      else
+        # Try to kill any existing processes, write pid, exit
         write_pid(pid, pid_file) if kill(pid, pid_file)
         exit
-      else
-        redirect_logs(stdout_file, stderr_file)
       end
     end
     
+    private
+    
     # Attempts to write the pid of the forked process to the pid file
-    def self.write_pid(pid, pidfile)
-      File.open(pidfile, "w") { |f| f.write(pid) }
+    def self.write_pid(pid, pid_file)
+      File.open(pid_file, "w") { |f| f.write(pid) }
+    rescue Errno::EPERM, Errno::EACCES
+      safe_pid_location = File.join(Dir.home, "lita.pid")
+      warn "Unable to write pid to: #{pid_file}.  Writing pid to #{safe_pid_location} instead."
+      File.open(safe_pid_location, "w") { |f| f.write(pid) }
     rescue ::Exception => e
       $stderr.puts "Unable to write pid file: unexpected #{e.class}: #{e}"
       Process.kill("QUIT", pid)
@@ -41,14 +48,24 @@ module Lita
     end
 
     # Redirect the stdout and stderr to log files
-    def self.redirect_logs(outfile, errfile)
-      $stdin.reopen('/dev/null')
-      out = File.new(outfile, "a")
-      err = File.new(errfile, "a")
-      $stdout.reopen(out)
-      $stderr.reopen(err)
-      $stdout.sync = true
-      $stderr.sync = true
+    def self.redirect_streams(outfile, errfile)
+      redirect_stream($stdin, '/dev/null', 'stdin', mode: 'r', sync: false)
+      redirect_stream($stdout, outfile, 'stdout')
+      redirect_stream($stderr, errfile, 'stderr')
     end
+    
+    def self.redirect_stream(stream, location, stream_name, mode: 'a', sync: true)
+      if File.writable?(location)
+        log_file = File.new(location, mode)
+      else
+        default_location = File.join(Dir.home, "lita.#{stream_name}.log")
+        warn "Unable to write to: #{location}. Writing to `#{default_location}' instead."
+        log_file = File.new(default_location, mode)
+      end
+      
+      stream.reopen(log_file)
+      stream.sync = sync
+    end
+    
   end
 end
